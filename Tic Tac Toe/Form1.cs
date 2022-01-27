65759d19d5690c78b25f44cc1656bdebd01a0d88
Nattawut Santhumpol <neslnwfaro003@hotmail.com>
2022-01-27 11:09:58 +0700

    Add project files.

---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        //ตัวแปร
+        bool turn = true;
+        bool against_computer = true;
+        int turn_count = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Nattawut", "About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+
+            //check to see if playing against computer and if it's O's turn
+            if((!turn) && (against_computer))
+            {
+                computer_make_move();
+            }
+
+            
+        }
+
+        private void checkForWinner()
+        {
+            bool there_is_a_winner = false;
+
+            //ตรวจสอบแนวนอน
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            //ตรวจสอบแนวตั้ง
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+            //ตรวจสอบแนวแยงมุม
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+            
+
+            if (there_is_a_winner)
+            {
+                //against_computer = false;
+                disableButtons();
+
+                String winner = "";
+                if (turn)
+                {
+                    winner = "O";
+                    o_count.Text = (Int32.Parse(o_count.Text) + 1).ToString();
+                    against_computer = false;
+                }
+                else
+                {
+                    winner = "X";
+                    x_count.Text = (Int32.Parse(x_count.Text) + 1).ToString();
+                    against_computer = false;
+                }
+                MessageBox.Show(winner + " Wins!", "Wow!");
+            }
+            else
+            {
+                if (turn_count == 9)
+                {
+                    MessageBox.Show("Drwa!", "Game Over!");
+                    draw_count.Text = (Int32.Parse(draw_count.Text) + 1).ToString();
+                    against_computer = false;
+                }
+            }
+        }
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach(Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+            foreach (Control c in Controls)
+            {
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                    against_computer = true;
+                }
+                catch { }
+            }
+            
+        }
+
+        private void button_enter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                if (turn)
+                    b.Text = "X";
+                else
+                    b.Text = "O";
+            }
+        }
+
+        private void button_leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.Enabled)
+            {
+                b.Text = "";
+            }
+        }
+
+        private void resetWinCountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            x_count.Text = "0";
+            o_count.Text = "0";
+            draw_count.Text = "0";
+            
+        }
+
+        private void computer_make_move()
+        {
+            //priority 1:  get tick tac toe
+            //priority 2:  block x tic tac toe
+            //priority 3:  go for corner space
+            //priority 4:  pick open space
+
+            Button move = null;
+
+            //look for tic tac toe opportunities
+            move = look_for_win_or_block("O"); //look for win
+            if (move == null)
+            {
+                move = look_for_win_or_block("X"); //look for block
+                if (move == null)
+                {
+                    move = look_for_corner();
+                    if (move == null)
+                    {
+                        move = look_for_open_space();
+                    }//end if
+                }//end if
+            }//end if
+
+            move.PerformClick();
+
+        }
+
+        private Button look_for_open_space()
+        {
+            Console.WriteLine("Looking for open space");
+            Button b = null;
+            foreach (Control c in Controls)
+            {
+                b = c as Button;
+                if (b != null)
+                {
+                    if (b.Text == "")
+                        return b;
+                }//end if
+            }//end if
+
+            return null;
+        }
+
+        private Button look_for_corner()
+        {
+            Console.WriteLine("Looking for corner");
+            if (A1.Text == "O")
+            {
+                if (A3.Text == "")
+                    return A3;
+                if (C3.Text == "")
+                    return C3;
+                if (C1.Text == "")
+                    return C1;
+            }
+
+            if (A3.Text == "O")
+            {
+                if (A1.Text == "")
+                    return A1;
+                if (C3.Text == "")
+                    return C3;
+                if (C1.Text == "")
+                    return C1;
+            }
+
+            if (C3.Text == "O")
+            {
+                if (A1.Text == "")
+                    return A3;
+                if (A3.Text == "")
+                    return A3;
+                if (C1.Text == "")
+                    return C1;
+            }
+
+            if (C1.Text == "O")
+            {
+                if (A1.Text == "")
+                    return A3;
+                if (A3.Text == "")
+                    return A3;
+                if (C3.Text == "")
+                    return C3;
+            }
+
+            if (A1.Text == "")
+                return A1;
+            if (A3.Text == "")
+                return A3;
+            if (C1.Text == "")
+                return C1;
+            if (C3.Text == "")
+                return C3;
+
+            return null;
+        }
+
+        private Button look_for_win_or_block(string mark)
+        {
+            Console.WriteLine("Looking for win or block:  " + mark);
+            //HORIZONTAL TESTS
+            if ((A1.Text == mark) && (A2.Text == mark) && (A3.Text == ""))
+                return A3;
+            if ((A2.Text == mark) && (A3.Text == mark) && (A1.Text == ""))
+                return A1;
+            if ((A1.Text == mark) && (A3.Text == mark) && (A2.Text == ""))
+                return A2;
+
+            if ((B1.Text == mark) && (B2.Text == mark) && (B3.Text == ""))
+                return B3;
+            if ((B2.Text == mark) && (B3.Text == mark) && (B1.Text == ""))
+                return B1;
+            if ((B1.Text == mark) && (B3.Text == mark) && (B2.Text == ""))
+                return B2;
+
+            if ((C1.Text == mark) && (C2.Text == mark) && (C3.Text == ""))
+                return C3;
+            if ((C2.Text == mark) && (C3.Text == mark) && (C1.Text == ""))
+                return C1;
+            if ((C1.Text == mark) && (C3.Text == mark) && (C2.Text == ""))
+                return C2;
+
+            //VERTICAL TESTS
+            if ((A1.Text == mark) && (B1.Text == mark) && (C1.Text == ""))
+                return C1;
+            if ((B1.Text == mark) && (C1.Text == mark) && (A1.Text == ""))
+                return A1;
+            if ((A1.Text == mark) && (C1.Text == mark) && (B1.Text == ""))
+                return B1;
+
+            if ((A2.Text == mark) && (B2.Text == mark) && (C2.Text == ""))
+                return C2;
+            if ((B2.Text == mark) && (C2.Text == mark) && (A2.Text == ""))
+                return A2;
+            if ((A2.Text == mark) && (C2.Text == mark) && (B2.Text == ""))
+                return B2;
+
+            if ((A3.Text == mark) && (B3.Text == mark) && (C3.Text == ""))
+                return C3;
+            if ((B3.Text == mark) && (C3.Text == mark) && (A3.Text == ""))
+                return A3;
+            if ((A3.Text == mark) && (C3.Text == mark) && (B3.Text == ""))
+                return B3;
+
+            //DIAGONAL TESTS
+            if ((A1.Text == mark) && (B2.Text == mark) && (C3.Text == ""))
+                return C3;
+            if ((B2.Text == mark) && (C3.Text == mark) && (A1.Text == ""))
+                return A1;
+            if ((A1.Text == mark) && (C3.Text == mark) && (B2.Text == ""))
+                return B2;
+
+            if ((A3.Text == mark) && (B2.Text == mark) && (C1.Text == ""))
+                return C1;
+            if ((B2.Text == mark) && (C1.Text == mark) && (A3.Text == ""))
+                return A3;
+            if ((A3.Text == mark) && (C1.Text == mark) && (B2.Text == ""))
+                return B2;
+
+            return null;
+        }
+
+    }
+}
